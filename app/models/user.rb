462b969eca5474d1c3b3896689ffd0ba7a0a6743
notsobad <tomomichi.onishi@gmail.com>
2025-01-11 14:53:14 +0900

    fix views

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,13 +11,13 @@ class User < ApplicationRecord
 
   enum :plan, { free_plan: "free", basic_plan: "basic" }
 
-  VALID_PASSWORD_PATTERN = "(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).{10,}"
+  VALID_PASSWORD_PATTERN = "(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).{8,}"
   validates :email_address, presence: true, uniqueness: true
   validates :password, confirmation: true, on: :create
   validates :password, presence: true,
             format: {
               with: /\A#{VALID_PASSWORD_PATTERN}\z/,
-              message: "は10文字以上で、英大文字・小文字・数字それぞれ1文字以上含む必要があります"
+              message: "は半角8文字以上で、英大文字・小文字・数字をそれぞれ1文字以上含む必要があります"
             }
   validates :password_confirmation, presence: true, on: :create
 
