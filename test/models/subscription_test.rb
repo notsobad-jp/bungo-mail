bdf0237cb8bfbfc1151e8a939bf394d15e163312
notsobad <tomomichi.onishi@gmail.com>
2025-01-17 00:41:02 +0900

    fix test

--- test/models/subscription_test.rb
+++ test/models/subscription_test.rb
@@ -16,42 +16,4 @@ describe Subscription do
       refute sub.valid?
     end
   end
-
-  describe "enabled_delivery_methods" do
-    context "user with free plan" do
-      context "with FCM token" do
-        it "should return [:webpush]" do
-          user = users(:free_with_fcm)
-          sub = Subscription.new(user: user)
-          assert_equal [:webpush], sub.enabled_delivery_methods
-        end
-      end
-
-      context "without FCM token" do
-        it "should return []" do
-          user = users(:free)
-          sub = Subscription.new(user: user)
-          assert_equal [], sub.enabled_delivery_methods
-        end
-      end
-    end
-
-    context "user with basic plan" do
-      context "with FCM token" do
-        it "should return [:email, :webpush]" do
-          user = users(:basic_with_fcm)
-          sub = Subscription.new(user: user)
-          assert_equal [:email, :webpush], sub.enabled_delivery_methods
-        end
-      end
-
-      context "without FCM token" do
-        it "should return [:email]" do
-          user = users(:basic)
-          sub = Subscription.new(user: user)
-          assert_equal [:email], sub.enabled_delivery_methods
-        end
-      end
-    end
-  end
 end
