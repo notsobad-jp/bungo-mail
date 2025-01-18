332d650a0ec1b8f17573afdb08903200a5f013a9
notsobad <tomomichi.onishi@gmail.com>
2025-01-19 02:05:50 +0900

    refactor subscribing campaigns

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,9 +18,4 @@ class User < ApplicationRecord
               message: "は半角8文字以上で、英大文字・小文字・数字をそれぞれ1文字以上含む必要があります"
             }
   validates :password_confirmation, presence: true, on: :create
-
-
-  def enabled_delivery_methods
-    Subscription::DELIVERY_METHOD_REQUIREMENTS.select { |_, requirement| requirement.call(self) }.keys
-  end
 end
