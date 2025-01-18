57f85fa57f2f63330dabfcc4b6dda20d84501241
notsobad <tomomichi.onishi@gmail.com>
2025-01-18 23:57:22 +0900

    validate unfinished campaigns count

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -25,15 +25,10 @@ class CampaignsController < ApplicationController
 
   def show
     @campaign = Campaign.find(params[:id])
-    @feeds = Feed.delivered_before(Time.current).where(campaign_id: @campaign.id).order(position: :desc).limit(10)
-    @subscription = current_user.subscriptions.find_or_initialize_by(campaign_id: @campaign.id) if authenticated?
+    @feeds = @campaign.feeds.delivered_before(Time.current).order(position: :desc).limit(10)
+    @subscription = @campaign.subscriptions.find_or_initialize_by(user: current_user)
     @meta_title = @campaign.author_and_book_name
     @breadcrumbs = [ {text: '配信管理', link: subscriptions_path}, {text: @meta_title} ] if @subscription
-
-    # 配信期間が重複している配信が存在してるかチェック
-    if authenticated? && current_user.id != @campaign.user_id
-      @overlapping_campaigns = current_user.subscribing_campaigns.where.not(id: @campaign.id).overlapping_with(@campaign.end_date, @campaign.start_date)
-    end
   end
 
   def destroy
